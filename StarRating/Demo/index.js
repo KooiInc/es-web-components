48de28fb6d6580c2eeec868aae0cab06b106a946
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-02 13:30:01 +0200

    Add star-rating module

---
+++ StarRating/Demo/index.js
@@ -0,0 +1,214 @@
+import {$, logFactory} from "//cdn.jsdelivr.net/gh/KooiInc/SBHelpers@main/index.browser.bundled.js";
+import createStarRatingComponent from "../index.js";
+
+createStarRatingComponent();
+
+const log2Screen = initialize();
+log2Screen(...intro());
+log2Screen(
+  `<star-rater
+      data-stars="4">
+     </star-rater>`,
+  
+  // data-initial="N" sets initial score to N
+  // data-button="1" creates button (reset to initial)
+  `<star-rater
+      data-stars="6"
+      data-initial="4"
+      data-button="1">
+     </star-rater>`,
+  
+  `!!<b>No or invalid <code>data-stars</code> values, defaults to 5 stars (see <code>limitsContract</code> in module code)</b>`,
+  
+  // no data-[stars/initial/button]: use defaults
+  `<star-rater/></star-rater>`,
+  
+  // data-stars not numeric (will revert to 5)
+  // data-initial out of bounds (will revert to 0)
+  `<star-rater
+      data-stars="not a number"
+      data-initial="20">
+    </star-rater>`,
+  
+  // data-stars and data-initial out of bounds
+  `<star-rater
+      data-initial="13"
+      data-stars="15">
+    </star-rater>`,
+  
+  // data-stars out of bounds
+  `<star-rater
+     data-stars="2"
+     data-initial="3">
+   </star-rater>`,
+  
+  `!!<b>How do you like it so far?</b>`,
+  
+  // data-button="1" creates button (reset to initial)
+  // (because data-initial has a value)
+  `<star-rater
+    data-stars="10"
+    data-initial="7"
+    data-button="1">
+   </star-rater>`,
+  
+  `<star-rater
+    data-stars="10">
+  </star-rater>`,
+);
+
+log2Screen(`!!<h2>A Questionnaire</h2>`, questionnaire());
+$.node(`[data-monitor]`).click();
+
+function questionnaire() {
+  const rater = `<star-rater data-stars="10" data-buttons="1" data-monitor="1"></star-rater>`;
+  const questions = [
+    `How much do you like programming?`,
+    `How much do you prefer ES20xx?`,
+    `Do you think ES20xx class syntax rocks?`,
+    `How's life today?` ];
+  const qDiv = $.virtual(`
+    <div class="q">
+      <div class="head">
+        Feel free to rate the following questions
+        (less to more, zero stars = don't know)
+      </div>
+      </div>`);
+  questions.forEach( q => qDiv.append(`<div>${q}${rater}</div>`));
+  qDiv.append(`<div class="qResult">Total: 0</div>`).append(`<p>&nbsp;</p>`);
+  
+  return qDiv.HTML.get(1);
+}
+
+function intro() {
+  return [
+    `!!<p>${/stackblitz/i.test(top.href) ?
+      `<a target="_top" href="//stackblitz.com/@KooiInc">All my projects</a><br>` : ``}
+    <a target="_blank" href="//github.com/KooiInc/es-webcomponent-factory"
+      >Web component module @GitHub</a>
+    <br><a target="_blank" href="https://stackoverflow.com/a/78731814/58186"
+      >StackOverflow answer</a></p>`,
+    `!!<h2>Ranking the stars</h2>
+    <div>
+      <p>Here is a web component (autonomous custom element) for creating star ratings.
+      <br>The custom element is <code>&lt;star-rater></code></p>
+      <p>After importing the creation factory (here as
+        <code>createStarRatingComponent</code>) you create the
+        custom element by calling it with or without limit
+        parameters: <code>{min: number, max: number, nDefault: number}</code>
+        where <code>min</code> is the minimum -,
+        <code>max</code> the maximum - and <code>nDefault</code> the
+        default amount of stars.
+      </p>
+      <p><b>The default limits are</b>:
+      <ul>
+        <li>maximum amount of stars (<code>max</code>): 10
+        <li>minimum amount of stars (<code>min</code>): 3
+        <li>default amount of stars (<code>nDefault</code>): 5
+      </ul></p>
+      <p>invalid or missing values are replaced with these defaults,
+          see examples.</p>
+      <p>
+        The <code>&lt;star-rater></code> element may contain the following data attributes:
+        <ul>
+          <li><code>data-stars="x"</code> - the number of stars the component should create</li>
+          <li><code>data-initial="x"</code> - the score the component should initially have</li>
+        </ul>
+        <p>The values are coompared with the aforementioned limits. When out of bounds,
+        they are substituted by default limit values.</p>
+      </p>
+      <p>
+        <b>Note</b>: the <code>&lt;star-rater></code> component can be created once.
+        Subsequent calls to the creator function will do nothing, apart from showing
+        a message in the console.
+       </p>
+    </div>`,
+    `<div class="starContainer">
+      <div class="stars">
+        <div class="star"></div>
+        <div class="star"></div>
+        <div class="star"></div>
+      </div>
+      By the way: I am <code><i>div.starContainer</i></code>.
+      Although a css class called <code>.starContainer</code> exists within
+      the web component (is encapsulated by it) it will only apply to the
+      contents of the component. For the document itself <i>div.starContainer</i>
+      has its own css-class (green color).
+      </div>`,
+    `!!<h2>Examples</h2>`
+  ];
+}
+
+function handleQ() {
+  const scores = $(`[data-monitor]`).collection;
+  const nQuestions = scores.length;
+  const nQuestionTotal = nQuestions * 10;
+  const score = scores.reduce( (acc, el) => acc + +el.dataset.score, 0);
+  document.querySelector(`.qResult`).textContent = `Total: ${score}/${
+    nQuestionTotal} (accumulated score: ${(+(score/nQuestions).toFixed(2)).toLocaleString()})`;
+}
+
+function initialize() {
+  const {log: log2Screen} = logFactory();
+  $.delegate(`click`, `[data-monitor]`, handleQ);
+  $.editCssRules(
+    `.container {
+      inset: 0;
+      position: absolute;
+      
+      #log2screen {
+        margin: 0 auto;
+        margin-bottom: 2rem;
+      }
+
+      @media(width <= 3200px) {
+        #log2screen {
+          max-width: 40vw;
+        }
+      }
+
+      @media(width <= 2400px) {
+        #log2screen {
+          max-width: 60vw;
+        }
+      }
+
+      @media(width <= 1024px) {
+        #log2screen {
+          max-width: 90vw;
+        }
+      }
+    }`,
+    `.starContainer { color: green; }`,
+    `.head div:first-child {
+      color: #777;
+      font-weight: normal;
+      margin-top: -0.5rem;
+    }`,
+    `#log2screen li div {
+      margin-top: 0
+    }`,
+    `.head ul {
+      margin-left: -0.5rem;
+    }`,
+    `#log2screen li div.q div:not(.head, .qResult) {
+      margin-top: 0.3rem;
+      font-style: italic;
+    }`,
+    `#log2screen li div.q div.head { margin-bottom: 1rem; }`,
+    `.qResult {
+      font-weight: bold;
+      color: #607ca1;
+      min-width: 320px;
+      max-width: 50vw;
+      white-space: nowrap;
+      padding: 4px;
+      background-color: #EEE;
+    }`,
+    `.q h3 { margin: 0.3em; }`
+  );
+  
+  $(`<div class="container">`).append($(`#log2screen`));
+  
+  return log2Screen;
+}
---
+++ StarRating/index.js
@@ -0,0 +1,227 @@
+import {
+  default as createComponent,
+  createOrRetrieveShadowRoot,
+  setComponentStyleFor,
+} from "../es-webcomponent-factory/Bundle/es-webcomponent-bundle.js";
+
+export default createStarRatingComponent;
+
+function createStarRatingComponent( { min, max, nDefault } = {} ) {
+  const { NR, inRange, gte, } = numberFactory();
+  const { componentName, componentStyle, createStar, limits, appStrings } =
+    retrieveComponentConstants({min, max, nDefault});
+  const [ add, remove, inActiveClass, resetQS, initialQS, div,
+    containerClass, starsClass, resetTtlAndClass, initialTtl,
+    initialClss, starsQS, starQS, containerQS, existsInfo ] = appStrings;
+  
+  if ( customElements.get(componentName) ) {
+    return console.info(existsInfo)
+  }
+  
+  createComponent({ componentName, onConnect, });
+  
+  function onConnect(componentNode) {
+    const shadow = createOrRetrieveShadowRoot(componentNode);
+    const starContainer = createStarContainerElement(componentNode);
+    shadow.append(starContainer);
+    shadow.adoptedStyleSheets = [setComponentStyleFor(componentNode, componentStyle)];
+    shadow.addEventListener(`click`, handleShadowrootClick);
+  }
+  
+  function handleShadowrootClick(evt) {
+    const origin = evt.target;
+    const shadowRoot = origin.getRootNode();
+    const host = shadowRoot.host;
+    const starsElement = shadowRoot.querySelector(starsQS);
+    const initialScore = NR(origin.dataset.setScore, -1);
+    const score = initialScore >= 0 ? initialScore : getScore(starsElement, origin);
+    const shouldChangeScore = NR(score, -1) >= 0 && NR(host.dataset.score) !== score;
+    
+    return  shouldChangeScore ? setStarRating(starsElement, host, score) : true;
+  }
+  
+  function createStarContainerElement(componentNode) {
+    checkLimits4AmountOfStars(componentNode);
+    const score = NR(componentNode.dataset.score);
+    const nStars = NR(componentNode.dataset.stars, limits.nDefault);
+    const starContainer = createElement(div, { className: containerClass });
+    const content = createElement( div, { className: starsClass }, { max: nStars, score } );
+    
+    starContainer.append(content);
+    starContainer.append(createElement( div, {
+      title: resetTtlAndClass, className: resetTtlAndClass }, { setScore: 0, stars: nStars } ) );
+    starContainer.append(createElement( div, {
+      title: initialTtl, className: initialClss }, {setScore: score, stars: nStars} ) );
+    
+    return setStarRating(content, componentNode, score);
+  }
+  
+  function checkLimits4AmountOfStars(componentNode) {
+    let nStars = NR(+componentNode.dataset?.stars, limits.nDefault);
+    const initialScore = NR(+componentNode.dataset.initial, 0);
+    const score = initialScore > 0 ? initialScore : NR(componentNode.dataset.score, 0);
+    nStars = inRange(nStars, limits.min, limits.max) ? nStars : limits.nDefault;
+    componentNode.dataset.stars = nStars;
+    componentNode.dataset.initial = +initialScore <= nStars ? initialScore : 0;
+    componentNode.dataset.score = +score <= nStars ? score : 0;
+  }
+  
+  function rateIt(rating, nStars) {
+    const result = [];
+    for (let i = 0; i < rating; i += 1) { result.push(createStar(true)); }
+    for (let i = 0; i < nStars - rating; i += 1) { result.push(createStar()); }
+    return result;
+  }
+  
+  function getScore(starsElement, clickedStar) {
+    if (starsElement === clickedStar) { return 0; }
+    
+    let score = 1;
+    
+    for (let star of starsElement.querySelectorAll(starQS)) {
+      if (star === clickedStar) {
+        return score;
+      }
+      
+      score += 1;
+    }
+  }
+  
+  function setStarRating(starsElement, host, score) {
+    const ratedStars = rateIt(score, +starsElement.dataset.max);
+    const stars = starsElement.querySelectorAll(starQS);
+    const nStars = ratedStars.length;
+    const starContainer = starsElement.closest(containerQS);
+    
+    for (let i = 0; i < nStars; i += 1) {
+      stars.length < 1 ? starsElement.append(ratedStars[i]) :
+        stars[i].replaceWith(ratedStars[i]);
+    }
+    
+    host.dataset.score = starsElement.dataset.score = score;
+    btnsActivation(starContainer, score, NR(host.dataset.initial));
+    
+    return starContainer;
+  }
+  
+  function btnsActivation(starContainer, score, initialValue) {
+    const reset = score < 1 ? add : remove;
+    const initial = initialValue === 0 ||
+    initialValue > 0 && score === initialValue ? add : remove;
+    starContainer.querySelector(resetQS).classList[reset](inActiveClass);
+    starContainer.querySelector(initialQS).classList[initial](inActiveClass);
+  }
+  
+  function createElement(name, props = {}, data = {}) {
+    const elem = Object.assign(document.createElement(name), props);
+    data = maybe( {trial: _ => Object.entries(data), whenError: _ => [] } );
+    
+    if (data.length) {
+      for (let [key, value] of data) {
+        elem.dataset[key] = value;
+      }
+    }
+    
+    return elem;
+  }
+  
+  function limitsContract(currentValues, defaults) {
+    const nDefault = NR(currentValues.nDefault, defaults.nDefault);
+    const rv = {
+      min: gte(NR(currentValues.min, defaults.min), defaults.min),
+      max: NR(currentValues.max, defaults.max),
+    };
+    
+    return {...rv, nDefault: inRange(nDefault, rv.min, rv.max) ? nDefault : defaults.nDefault};
+  }
+  
+  function numberFactory() {
+    const isNumber = v => v?.constructor !== Array && (+v).constructor === Number && !isNaN(+v) && +v !== Infinity;
+    const numberOrDefault = (v, defaultValue2Return) =>
+      isNumber(v) ? +v : isNumber(defaultValue2Return) ? +defaultValue2Return : 0;
+    const gte = (v, gtv) => {
+      v = numberOrDefault(v, -1); gtv = numberOrDefault(gtv);
+      return v >= gtv ? v : gtv;
+    };
+    const inRange = (v, min, max) => {
+      v = numberOrDefault(v, -1); min = numberOrDefault(min); max = numberOrDefault(max);
+      return v >= min && v <= max;
+    };
+    
+    return { NR: numberOrDefault, gte, inRange, };
+  }
+  
+  function retrieveComponentConstants(starLimits) {
+    const limitsDefault = { min: 3, max: 10, nDefault: 5 };
+    const componentName = `star-rater`;
+    const existsInfo = `✔ The <star-rater> web component can only be created once. `+
+      `It already exists and is ready for use. Enjoy!`
+    const btnActivationStrings = [`add`, `remove`, `inactive`, `.reset`, `.initial`];
+    const appStrings =  [ `div`, `starContainer`, `stars`, `reset`, `revert to initial`, `initial`,
+      `.stars`, `.star`, `.starContainer` ];
+    
+    return {
+      limits: limitsContract( starLimits , limitsDefault ),
+      appStrings: btnActivationStrings.concat(appStrings).concat(existsInfo),
+      componentName,
+      componentStyle: `
+          :host {
+            font-weight: normal;
+            display: block;
+            font-style: normal;
+            .initial:after, .reset:after {
+              letter-spacing: initial;
+              font-weight: bold;
+              color: #c45525;
+              background-color: #EEE;
+              padding: 2px 4px;
+              position: relative;
+              text-align: center;
+              margin-left: 5px;
+              top: -5px;
+              cursor: pointer;
+              min-width: 48px;
+            }
+            .starContainer {
+              padding: 0.4rem 0;
+              display: inline-block;
+              .initial, .reset { display: inline-block; }
+              .reset:after { content: '\u21B6 'attr(data-set-score)'/'attr(data-stars); }
+              .initial:after { content: '↺ 'attr(data-set-score)'/'attr(data-stars); }
+              .reset.inactive, .initial.inactive { display: none;  }
+              .stars {
+                display: inline-block;
+                cursor: pointer;
+                letter-spacing: 2px;
+                .star {
+                  font-size: 2rem;
+                  -webkit-text-stroke: 1px #999;
+                  text-shadow: 1px 1px 4px #999;
+                  color: white;
+                  &:before { content: '★'; }
+                  &.rated:before { color: gold; }
+                  &:hover:before {
+                    color: gold;
+                    -webkit-text-stroke: 1px #c45525;
+                    text-shadow: 1px 1px 4px #c45525;
+                  }
+                }
+              }
+            }
+          }`,
+      createStar(rated) { return createElement(`span`, {className: `star${rated ? ` rated` : ``}`}); },
+    };
+  }
+  
+  function maybe({trial, whenError = err => console.log(err)} = {}) {
+    try {
+      if (trial?.constructor !== Function) {
+        throw new TypeError(`maybe: trial parameter not a Function or Lambda`);
+      }
+      
+      return trial();
+    } catch (err) {
+      return whenError?.constructor === Function ? whenError(err) : console.error(err);
+    }
+  }
+}
