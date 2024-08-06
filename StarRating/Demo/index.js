9a52abbf6ba0c89029541c7cedb115a325f679f0
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-06 12:11:41 +0200

    Use expandable-text for info

--- StarRating/Demo/index.js
+++ StarRating/Demo/index.js
@@ -1,10 +1,10 @@
-import {$, logFactory} from "//cdn.jsdelivr.net/gh/KooiInc/SBHelpers@main/index.browser.bundled.js";
+import {$, logFactory} from "//cdn.jsdelivr.net/gh/KooiInc/SBHelpers@main/index.browser.js?version=makeitso";
 import createStarRatingComponent from "../index.js";
 
 createStarRatingComponent();
 
 const log2Screen = initialize();
-log2Screen(...intro());
+log2Screen(`!!<h2>Examples</h2>`);
 log2Screen(
   `<star-rater
       data-stars="4">
@@ -80,69 +80,6 @@ function questionnaire() {
   return qDiv.HTML.get(1);
 }
 
-function intro() {
-  return [
-    `!!<p>
-      ${/stackblitz/i.test(top.location.href) ?
-      `<a target="_blank" href="//stackblitz.com/@KooiInc"
-          >All my projects</a><br>` : ``}
-      ${/kooiinc\.github\.io/i.test(top.location.href) ?
-        `<a target="_top" href="https://github.com/KooiInc/es-web-components"
-            >GitHub repository</a><br>` : ``}
-      <a target="_blank" href="//github.com/KooiInc/es-webcomponent-factory"
-        >Web component module @GitHub</a>
-     </p>`,
-    `!!<h2>Ranking the stars</h2>
-    <div>
-      <p>Here is a web component (autonomous custom element) for creating star ratings.
-      <br>The custom element is <code>&lt;star-rater></code></p>
-      <p>After importing the creation factory (here as
-        <code>createStarRatingComponent</code>) you create the
-        custom element by calling it with or without limit
-        parameters: <code>{min: number, max: number, nDefault: number}</code>
-        where <code>min</code> is the minimum -,
-        <code>max</code> the maximum - and <code>nDefault</code> the
-        default amount of stars.
-      </p>
-      <p><b>The default limits are</b>:
-      <ul>
-        <li>maximum amount of stars (<code>max</code>): 10
-        <li>minimum amount of stars (<code>min</code>): 3
-        <li>default amount of stars (<code>nDefault</code>): 5
-      </ul></p>
-      <p>invalid or missing values are replaced with these defaults,
-          see examples.</p>
-      <p>
-        The <code>&lt;star-rater></code> element may contain the following data attributes:
-        <ul>
-          <li><code>data-stars="x"</code> - the number of stars the component should create</li>
-          <li><code>data-initial="x"</code> - the score the component should initially have</li>
-        </ul>
-        <p>The values are coompared with the aforementioned limits. When out of bounds,
-        they are substituted by default limit values.</p>
-      </p>
-      <p>
-        <b>Note</b>: the <code>&lt;star-rater></code> component can be created once.
-        Subsequent calls to the creator function will do nothing, apart from showing
-        a message in the console.
-       </p>
-    </div>`,
-    `<div class="starContainer">
-      <div class="stars">
-        <div class="star"></div>
-        <div class="star"></div>
-        <div class="star"></div>
-      </div>
-      By the way: I am <code><i>div.starContainer</i></code>.
-      Although a css class called <code>.starContainer</code> exists within
-      the web component (is encapsulated by it) it will only apply to the
-      contents of the component. For the document itself <i>div.starContainer</i>
-      has its own css-class (green color).
-      </div>`,
-    `!!<h2>Examples</h2>`
-  ];
-}
-
 function handleQ() {
   const scores = $(`[data-monitor]`).collection;
   const nQuestions = scores.length;
@@ -153,7 +90,7 @@ function handleQ() {
 }
 
 function initialize() {
-  const {log: log2Screen} = logFactory();
+  const {log: log2Screen, logTop} = logFactory();
   $.delegate(`click`, `[data-monitor]`, handleQ);
   $.editCssRules(
     `.container {
@@ -212,7 +149,19 @@ function initialize() {
     `.q h3 { margin: 0.3em; }`
   );
   
-  $(`<div class="container">`).append($(`#log2screen`));
+  const logScreen = $(`#log2screen`);
+  $(`<div class="container">`).append(logScreen);
+  logScreen.prepend($(`<li class="head">`).append($(`expandable-text`)));
+  logTop(`!!<p>
+      ${/stackblitz/i.test(top.location.href) ?
+    `<a target="_blank" href="//stackblitz.com/@KooiInc"
+          >All my projects</a><br>` : ``}
+      ${/kooiinc\.github\.io/i.test(top.location.href) ?
+    `<a target="_top" href="https://github.com/KooiInc/es-web-components"
+            >GitHub repository</a><br>` : ``}
+      <a target="_blank" href="//github.com/KooiInc/es-webcomponent-factory"
+        >Web component module @GitHub</a>
+     </p>`);
   
   return log2Screen;
 }
