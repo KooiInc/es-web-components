2f78a0960a10f57fd6b73fa551abc17730849ac9
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-06 12:11:00 +0200

    Use common helpers

--- StarRating/index.js
+++ StarRating/index.js
@@ -1,3 +1,4 @@
+import {createElement, numberFactory,} from "../Common/CommonHelpers.js";
 import {
   default as createComponent,
   createOrRetrieveShadowRoot,
@@ -112,19 +113,6 @@ function createStarRatingComponent( { min, max, nDefault } = {} ) {
     starContainer.querySelector(initialQS).classList[initial](inActiveClass);
   }
   
-  function createElement(name, props = {}, data = {}) {
-    const elem = Object.assign(document.createElement(name), props);
-    data = maybe( {trial: _ => Object.entries(data), whenError: _ => [] } );
-    
-    if (data.length) {
-      for (let [key, value] of data) {
-        elem.dataset[key] = value;
-      }
-    }
-    
-    return elem;
-  }
-  
   function limitsContract(currentValues, defaults) {
     const nDefault = NR(currentValues.nDefault, defaults.nDefault);
     const rv = {
@@ -133,22 +121,6 @@ function createStarRatingComponent( { min, max, nDefault } = {} ) {
     };
     
     return {...rv, nDefault: inRange(nDefault, rv.min, rv.max) ? nDefault : defaults.nDefault};
-  }
-  
-  function numberFactory() {
-    const isNumber = v => v?.constructor !== Array && (+v).constructor === Number && !isNaN(+v) && +v !== Infinity;
-    const numberOrDefault = (v, defaultValue2Return) =>
-      isNumber(v) ? +v : isNumber(defaultValue2Return) ? +defaultValue2Return : 0;
-    const gte = (v, gtv) => {
-      v = numberOrDefault(v, -1); gtv = numberOrDefault(gtv);
-      return v >= gtv ? v : gtv;
-    };
-    const inRange = (v, min, max) => {
-      v = numberOrDefault(v, -1); min = numberOrDefault(min); max = numberOrDefault(max);
-      return v >= min && v <= max;
-    };
-    
-    return { NR: numberOrDefault, gte, inRange, };
   }
   
   function retrieveComponentConstants(starLimits) {
@@ -211,17 +183,5 @@ function createStarRatingComponent( { min, max, nDefault } = {} ) {
           }`,
       createStar(rated) { return createElement(`span`, {className: `star${rated ? ` rated` : ``}`}); },
     };
-  }
-  
-  function maybe({trial, whenError = err => console.log(err)} = {}) {
-    try {
-      if (trial?.constructor !== Function) {
-        throw new TypeError(`maybe: trial parameter not a Function or Lambda`);
-      }
-      
-      return trial();
-    } catch (err) {
-      return whenError?.constructor === Function ? whenError(err) : console.error(err);
-    }
   }
 }
