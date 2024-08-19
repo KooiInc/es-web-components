8efbcc3c12769f5ddc22a152edfe6b121f97ba12
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-19 15:56:32 +0200

    PasswordHelper demo inline styling

--- PasswordHelper/Demo/index.js
+++ PasswordHelper/Demo/index.js
@@ -1,39 +1,9 @@
 import {$} from "//cdn.jsdelivr.net/gh/KooiInc/SBHelpers@latest/index.browser.js";
-import "../index.js";
-
 $.editCssRules(
-  `body {
-    font: normal 14px/19px verdana, arial, sans-serif;
-  }`,
-  `.container {
-    position: absolute;
-    inset: 0;
-    
-    .pageContent {
-      width: 50vw;
-      padding-top: 2rem;
-      margin: 0 auto;
-    }
-    
-    @media(width <= 3200px) {
-      .pageContent {
-        width: 33vw;
-      }
-    }
 
-    @media(width <= 2400px) {
-      .pageContent {
-        width: 43vw;
-      }
-    }
-
-    @media(width <= 1024px) {
-      .pageContent {
-        width: 90vw;
-      }
-    }
-  }`
 );
+
+import "../index.js";
 
 $(`.container .pageContent`).prepend(
   $(`<p>
