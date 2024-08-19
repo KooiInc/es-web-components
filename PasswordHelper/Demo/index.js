77c9c1d47bb05d937bf4e49b3b3571cbe768b9c0
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-19 15:53:48 +0200

    PasswordHelper demo top links

--- PasswordHelper/Demo/index.js
+++ PasswordHelper/Demo/index.js
@@ -33,4 +33,13 @@ $.editCssRules(
       }
     }
   }`
-)
+);
+
+$(`.container .pageContent`).prepend(
+  $(`<p>
+      ${/kooiinc\.github\.io/i.test(top.location.href) ?
+    `<a target="_top" href="https://github.com/KooiInc/es-web-components/">GitHub</a><br>` : ``}
+      <a target="_blank" href="//github.com/KooiInc/es-webcomponent-factory"
+        >Web component module @GitHub</a>
+    </p>`)
+);
