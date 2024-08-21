bcf1ffafebd50760b3af130cc1f87e4111dc291a
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-21 18:04:33 +0200

    PasswordHelper report less than a day duration

--- PasswordHelper/AppText.js
+++ PasswordHelper/AppText.js
@@ -111,7 +111,8 @@ const reportEntropyTexts = {
   wouldTake: {EN: `that would take`, NL: `zou dat`},
   years: {EN: `year(s)`, NL: `jaar`},
   days: {EN: `day(s)`, NL: `dag(en)`},
-  vergen: {EN: ``, NL: `vergen`},
+  lessThanADay: {EN: `less than a day`, NL: `minder dan een dag`},
+  vergen: {EN: ``, NL: `duren`},
   atLeast: {
     EN: `A hacker needs to guess <i>at least</i>`,
     NL: `Een hacker heeft <i>tenminste</i>`},
@@ -121,13 +122,15 @@ const reportEntropyTexts = {
 }
 
 function inWordsLanguage(calculated, years, days, lang = `EN`) {
+  const duration = years < 1 && days < 1
+    ? `<b><i>${reportEntropyTexts.lessThanADay[lang]}</i></b>` :
+    `${years.toLocaleString(lang)} ${reportEntropyTexts.years[lang]} ${
+        reportEntropyTexts.and[lang]} ${ days} ${reportEntropyTexts.days[lang]}`;
   return calculated.entropy <= 2 ? `` : `${reportEntropyTexts.thats[lang]} ${
     inWords(calculated.intruderGuessAttempts, calculated.entropy, lang)}
       ${reportEntropyTexts.what[lang]} ${(100_000).toLocaleString(lang)} per ${
         reportEntropyTexts.second[lang]} ${reportEntropyTexts.wouldTake[lang]} ${
-        years.toLocaleString(lang)} ${reportEntropyTexts.years[lang]} ${
-          reportEntropyTexts.and[lang]} ${ days} ${reportEntropyTexts.days[lang]} ${
-            reportEntropyTexts.vergen[lang]}`;
+          duration} ${reportEntropyTexts.vergen[lang]}`;
 }
 
 function inWords(intruderGuessAttempts, entropy, lang) {
