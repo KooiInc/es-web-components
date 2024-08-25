90a43c92083285afb82a5d4dff2ac5746a62d37e
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-25 15:30:58 +0200

    PasswordHelper demo b&b

--- PasswordHelper/Demo/index.js
+++ PasswordHelper/Demo/index.js
@@ -1,42 +1,33 @@
 import "../index.js";
-import {$} from "https://cdn.jsdelivr.net/gh/KooiInc/SBHelpers@latest/index.browser.bundled.js";
+import $ from "https://cdn.jsdelivr.net/gh/KooiInc/JQL@latest/Bundle/jql.min.js";
 buildPage();
 
 function buildPage() {
-  $.editCssRules(`pre {margin: 0.2rem auto;}`, `.hidden {display: none;}`);
-  const body = $(document.body).addClass(`hidden`);
-  fetch(`./demoPrefix.html`)
-    .then(r => r.text())
-    .then(createElements)
-    .then(_ =>
-      fetch(`./demoPageStyle.css`)
-        .then(r => r.text())
-        .then(r => $.node(`head`).insertAdjacentHTML(`beforeend`, `<style id="local">${r}</style>`))
-        .then(_ => body.removeClass(`hidden`) )
-    );
+  fetch(`./demoPrefix.html`).then(async r =>
+    createElements(await r.text(), $(document.body).addClass(`hidden`)));
 }
 
-function createElements(template) {
+function createElements(template, body) {
   $.allowTag(`template`);
-  $(`<div class="container">
-        <div class="pageContent">${template}</div>
-     </div`);
+  const repoBackLink = /kooiinc\.github\.io/i.test(top.location.href) ?
+    `<a target="_top" href="https://github.com/KooiInc/es-web-components/">GitHub</a><br>` : ``
+  $(`<div class="container"><div class="pageContent">${template}</div></div`);
   $(`.pageContent`).prepend($(`
-      <p>
-        ${/kooiinc\.github\.io/i.test(top.location.href) ?
-    `<a target="_top" href="https://github.com/KooiInc/es-web-components/">GitHub</a><br>` : ``}
-        <a target="_blank" href="//github.com/KooiInc/es-webcomponent-factory">Web component module @GitHub</a>
-      </p>`) );
+      <p>${repoBackLink}<a target="_blank" href="//github.com/KooiInc/es-webcomponent-factory"
+        >Web component module @GitHub</a></p>`) );
   $(`.container`).append($(`<password-helper data-language="EN">`));
   $.node(`expandable-text#prefix`).shadowRoot.addEventListener('click', handle);
+  $.editCssRules(`pre {margin: 0.2rem auto;}`, `.hidden {display: none;}`);
+  fetch(`./demoPageStyle.css`)
+    .then(async r => $(`head`).append($(`<style id="local">${await r.text()}</style>`)))
+    .then(_ => (body.removeClass(`hidden`), console.info(`✓ Demo page done`)) );
 }
 
 function handle(evt) {
   if (evt.target.dataset.click) {
-    const styling = $.node(`#local`);
     $.Popup.show( {
       content: $(`<pre>${
-        styling.innerHTML
+        $(`#local`).html()
           .replace(/password-helper/g, `<b style="color:red">password-helper</b>`)
           .trim()}</pre>`)} );
   }
